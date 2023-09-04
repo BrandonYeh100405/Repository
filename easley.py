78c16e83c4752d1fbcddbd9be3c161b87a35c401
BrandonYeh100405 <142930815+BrandonYeh100405@users.noreply.github.com>
2023-09-04 16:26:21 -0700

    Update and rename hello-world.py to easley.py

---
+++ easley.py
@@ -0,0 +1,5 @@
+print("Welcome to Space Travel Calculator")
+distance = input("What is the distance")
+speed = input("What is the speed")
+time = distance / speed
+print("It would take" + time + "minutes to reach the destination")
